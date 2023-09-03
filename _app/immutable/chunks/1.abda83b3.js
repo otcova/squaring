2c6466af29cc41585f747e072682bde91365eac7
xiangfengyepan <xiangfeng.yepan@gmail.com>
2023-09-03 11:33:56 +0200

    Updates

---
+++ _app/immutable/chunks/1.abda83b3.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.26849b99.js";export{t as component};
--- _app/immutable/chunks/paths.8086f377.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_17nlt0x)==null?void 0:s.base)??"/squaring";var t;const e=((t=globalThis.__sveltekit_17nlt0x)==null?void 0:t.assets)??a;export{e as a,a as b};
